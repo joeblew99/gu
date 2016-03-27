1c8a3dcc511871daef94cc91e1665bcbdbc6e607
Alexander Ewetumo <trinoxf@gmail.com>
2016-03-27 18:32:49 +0100

    iRemoved casowary to dev

--- gucassowary/doc.go
+++
@@ -1,4 +0,0 @@
-// Package gucassowary provides a cassowary constraint solver for the gu UI
-// library, which is a efficient, but general purpose implementation of the
-// cassowary constaint solver for User interfaces.
-package gucassowary
