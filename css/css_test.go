5e13d5cad55aadee2589fff6d5bdfdc7cad80028
Alexander Ewetumo <trinoxf@gmail.com>
2016-10-30 23:49:13 +0100

    adding basic css package

--- css/css_test.go
+++ css/css_test.go
@@ -51,3 +51,55 @@ func TestBasicCSS(t *testing.T) {
 	}
 	tests.Passed(t, "Should have rendered expected stylesheet")
 }
+
+func TestLinkedCSS(t *testing.T) {
+	expected := "#galatica block {\n  Helvetica\n      color: Pink;\n}\n#galatica::before {\n  content: \"bugger\";\n}\n#galatica div a {\n  color: black;\n  font-family: Helvetica;\n}\n@media (max-width: 400px) {\n  #galatica:hover {\n    color: blue;\n    font-family: Helvetica;\n  }\n}"
+
+	csr := css.NewRule(`
+    block {
+      font-family: {{ .Font }}
+      color: {{ .Color }}
+    }
+  `)
+
+	csx := css.NewRule(`
+
+    ::before {
+      content: "bugger";
+    }
+
+    div a {
+      color: black;
+      font-family: {{ .Font }}
+    }
+
+    @media (max-width: 400px){
+
+      :hover {
+        color: blue;
+        font-family: {{ .Font }}
+      }
+
+    }
+`, csr)
+
+	sheet, err := csx.Stylesheet(struct {
+		Font  string
+		Color string
+	}{
+		Font:  "Helvetica",
+		Color: "Pink",
+	}, "#galatica")
+
+	if err != nil {
+		tests.Failed(t, "Should have successfully processed stylesheet for rule")
+	}
+	tests.Passed(t, "Should have successfully processed stylesheet for rule")
+
+	if val := sheet.String(); val != expected {
+		t.Logf("\t\tRecieved: %q\n", val)
+		t.Logf("\t\tExpected: %q\n", expected)
+		tests.Failed(t, "Should have rendered expected stylesheet")
+	}
+	tests.Passed(t, "Should have rendered expected stylesheet")
+}
